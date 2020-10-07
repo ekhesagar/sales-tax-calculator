1504b1aa3774c692a066cef78f3412468ead3dd9
Sagar Ekhe <sagar@Sagars-MacBook-Air.local>
2020-10-07 10:44:48 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
+import Form from './components/Form';
+import Receipt from './components/Receipt';
 import './App.css';
 
-function App() {
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      count: 0,
+      type: String('regular'),
+      product: String('book'),
+      price: 0,
+      items: [],
+      totalTax: 0,
+      total: 0
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+handleChange(e) {
+  this.setState({
+    [e.target.name] : e.target.value
+  })
+}
+
+handleSubmit(e) {
+  e.preventDefault();
+  const taxExempted = ['book', 'headache pills', 'chocolates'];
+
+  let total, totalTax
+  let price = this.state.price*this.state.count;
+  let product = this.state.product;
+  if(this.state.type === 'imported'){
+    if(taxExempted.includes(product)) price = price+ price*(0.05)
+    else price = price + price*(0.15)
+  }else if(!taxExempted.includes(product) && this.state.type !== 'imported') price = price + price * 0.1
+
+  total = this.state.total + price;
+  totalTax = (price - this.state.price) + this.state.totalTax;
+  let item = {
+    count: this.state.count,
+    type: this.state.type,
+    product: this.state.product,
+    totalPrice: price,
+    price: this.state.price
+  }
+  this.setState( prev => ({
+    ...prev,
+    items: [...this.state.items, item],
+    total: total,
+    totalTax: totalTax
+  }))
+  
+}
+
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form 
+        handleChange={this.handleChange}
+        handleSubmit={this.handleSubmit}
+      />
+      <Receipt itemList={this.state.items} />
     </div>
-  );
+  )
+}
 }
 
 export default App;
+
+
